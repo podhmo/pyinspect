625e504ca8c6a8789784c24bf67e4756ec065804
podhmo <ababjam61+github@gmail.com>
2018-07-12 01:22:55 +0900

    drop pydoc dependencies

--- pyinspect/inspect.py
+++ pyinspect/inspect.py
@@ -1,11 +1,26 @@
-import pydoc
-import re
+from inspect import signature, classify_class_attrs
+from logging import getLogger as get_logger
+logger = get_logger(__name__)
 
 
-def shape_text(this_cls, doc=pydoc.plaintext, skip_special_method=False, skip_private_method=False):
+def _indent(text, prefix='    '):
+    """Indent text by prepending a given prefix to each line."""
+    if not text:
+        return ''
+    lines = [prefix + line for line in text.split('\n')]
+    if lines:
+        lines[-1] = lines[-1].rstrip()
+    return '\n'.join(lines)
+
+
+def doc(name, ob):
+    # pydoc.plaintext.doc(ob)
+    return f"{name}{signature(ob)}"
+
+
+def shape_text(this_cls, skip_special_method=False, skip_private_method=False):
     attrs = [
-        (name, kind) for name, kind, cls, _ in pydoc.classify_class_attrs(this_cls)
-        if cls == this_cls
+        (name, kind) for name, kind, cls, _ in classify_class_attrs(this_cls) if cls == this_cls
     ]
 
     if skip_special_method:
@@ -18,24 +33,25 @@ def shape_text(this_cls, doc=pydoc.plaintext, skip_special_method=False, skip_pr
         attrs = [
             (name, kind) for name, kind in attrs if not name.startswith("_") or name.endswith("__")
         ]
-    method_docs = [
-        "[{kind}{prefix}] {doc}".format(
-            prefix=", OVERRIDE" if any(c for c in this_cls.mro()[1:] if hasattr(c, name)) else "",
-            kind=kind,
-            doc=doc.document(getattr(this_cls, name))
-        ) for name, kind in attrs if "method" in kind
-    ]
+    method_docs = []
+    for name, kind in attrs:
+        if "method" not in kind:
+            continue
+        prefix = ", OVERRIDE" if any(c for c in this_cls.mro()[1:] if hasattr(c, name)) else ""
 
-    content = doc.indent("".join(method_docs))
+        try:
+            content = doc(name, getattr(this_cls, name))
+        except ValueError as e:
+            logger.info(e)
+            continue
+
+        method_docs.append(
+            "[{kind}{prefix}] {content}".format(prefix=prefix, kind=kind, content=content)
+        )
+
+    content = _indent("\n".join(method_docs))
     relation = " <- ".join([f"{cls.__module__}.{cls.__name__}" for cls in this_cls.mro()])
     return "\n".join([relation, content])
-
-
-def grep_by_indent(s, level, rx=re.compile("^\s+")):
-    for line in s.split("\n"):
-        m = rx.search(line)
-        if m is None or len(m.group(0)) <= level:
-            yield line
 
 
 def inspect(target, io=None, skip_special_method=False, skip_private_method=False, only_this=False):
@@ -45,6 +61,6 @@ def inspect(target, io=None, skip_special_method=False, skip_private_method=Fals
         text = shape_text(
             cls, skip_special_method=skip_special_method, skip_private_method=skip_private_method
         )
-        print("\n".join(grep_by_indent(text, 4)), file=io)
+        print(text, file=io)
         if only_this:
             break
