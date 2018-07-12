262e83b44ecc911e23b98160d9a8cab985a40131
podhmo <ababjam61+github@gmail.com>
2018-07-13 02:24:48 +0900

    test: adding inspect test

--- pyinspect/tests/__init__.py
+++ pyinspect/tests/__init__.py
@@ -4,6 +4,11 @@ logger = get_logger(__name__)
 
 
 class TestCaseWithDebugLogging(unittest.TestCase):
+    def assertEqual(self, got, expected, msg=None):
+        logger.debug("got %s", got)
+        logger.debug("expected %s", expected)
+        return super().assertEqual(got, expected, msg=msg)
+
     def assertDictEqual(self, got, expected, msg=None):
         logger.debug("got %s", got)
         logger.debug("expected %s", expected)
---
+++ pyinspect/tests/test_inspect.py
@@ -0,0 +1,46 @@
+import textwrap
+from pyinspect.tests import TestCaseWithDebugLogging
+
+
+class Tests(TestCaseWithDebugLogging):
+    def _callFUT(self, cls):
+        from io import StringIO
+        from pyinspect.inspect import Options
+        from pyinspect.inspect import inspect as target_function
+        o = StringIO()
+        target_function(cls, options=Options(), io=o)
+        return o.getvalue()
+
+    def test_it(self):
+        class F:
+            def init(self, x, *, y=None):
+                self.init_x(x)
+                self.init_y(y=y)
+
+            def init_x(self, x):
+                self.init_x_z()
+
+            def init_x_z(self, x):
+                pass
+
+            def init_y(self, *, y=None):
+                pass
+
+        got = self._callFUT(F)
+        expected = textwrap.dedent(
+            f"""\
+        {__name__}.F <- builtins.object
+            [method] init(self, x, *, y=None)
+            [method] init_x(self, x)
+            [method] init_x_z(self, x)
+            [method] init_y(self, *, y=None)
+        """
+        )
+        self.assertEqual(got=got.strip("\n"), expected=expected.strip("\n"))
+
+
+if __name__ == "__main__":
+    import unittest
+    import logging
+    logging.basicConfig(level=logging.DEBUG)
+    unittest.main()
